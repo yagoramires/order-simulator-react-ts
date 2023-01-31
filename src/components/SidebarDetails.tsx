2af2504cac56b52b50e07354fab96ba84ba86d8c
yagox <yago.ramiresx@gmail.com>
2023-01-31 20:51:00 -0300

    fix: styles

--- src/components/SidebarDetails.tsx
+++ src/components/SidebarDetails.tsx
@@ -22,7 +22,7 @@ const SidebarDetails = ({ industry }: industryIndustry) => {
   return (
     <aside className='flex flex-col gap-16 items-center justify-between md:justify-center p-4 md:p-0 bg-white md:fixed md:left-0 md:right-0 md:bottom-0 md:z-[999] md:w-full md:flex-row w-[450px] shadow-lg'>
       <div className='flex flex-col items-center justify-center gap-4 '>
-        <div className='flex items-center justify-between w-full text-4xl font-bold text-blue-600 md:text-2xl'>
+        <div className='flex items-center justify-between w-full text-4xl font-bold text-blue-600 md:text-2xl md:p-4'>
           <Link to='/dashboard/industries'>
             <MdKeyboardArrowLeft />
           </Link>
