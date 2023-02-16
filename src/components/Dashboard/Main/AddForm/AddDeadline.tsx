4aa31476665df564f6ad91d4a61549bd74850000
yagox <yago.ramiresx@gmail.com>
2023-02-15 21:39:05 -0300

    styles: ajustes nos formularios de adicao

--- src/components/Dashboard/Main/AddForm/AddDeadline.tsx
+++ src/components/Dashboard/Main/AddForm/AddDeadline.tsx
@@ -37,8 +37,8 @@ const AddDeadline = () => {
       </Dialog.Trigger>
       <Dialog.Portal>
         <Dialog.Overlay className='fixed inset-0 bg-black/80' />
-        <Dialog.Content className='fixed bg-white top-1/2 w-[90%] max-w-[800px] rounded-md shadow-lg p-8 translate-x-[-50%] translate-y-[-50%] left-1/2'>
-          <Dialog.Title className='flex justify-between mb-4 font-bold text-blue-600'>
+        <Dialog.Content className='fixed top-1/2 w-[90%] max-w-[800px] rounded-lg shadow-md p-8 translate-x-[-50%] translate-y-[-50%] left-1/2 z-10 bg-gray-800'>
+          <Dialog.Title className='flex justify-between mb-4 font-bold text-gray-50'>
             <Dialog.Close className='flex justify-between w-full mb-4'>
               Adicionar Prazo de pagamento
               <MdClose className='text-xl' />
@@ -47,14 +47,14 @@ const AddDeadline = () => {
           <form className='flex flex-col w-full gap-4' onSubmit={handleDeadline}>
             <input
               type='text'
-              className='p-2 bg-gray-300 rounded-md shadow-sm'
+              className='w-full p-2 bg-gray-900 rounded-lg text-gray-50'
               placeholder='Prazo de pagamento'
               value={value}
               onChange={(e) => setValue(e.target.value)}
             />
             <input
               type='submit'
-              className='p-2 font-medium text-white rounded-md shadow-sm cursor-pointer bg-gradient-to-r from-blue-800 to-blue-600'
+              className='p-2 font-bold bg-blue-600 rounded-md shadow-sm cursor-pointer text-gray-50'
               value={'Adicionar'}
             />
           </form>
--- src/components/Dashboard/Main/AddForm/AddIndustry.tsx
+++ src/components/Dashboard/Main/AddForm/AddIndustry.tsx
@@ -43,8 +43,8 @@ const AddIndustry = () => {
       </Dialog.Trigger>
       <Dialog.Portal>
         <Dialog.Overlay className='fixed inset-0 bg-black/80' />
-        <Dialog.Content className='fixed bg-white top-1/2 w-[90%] max-w-[800px] rounded-md shadow-lg p-8 translate-x-[-50%] translate-y-[-50%] left-1/2'>
-          <Dialog.Title className='flex justify-between mb-4 font-bold text-blue-600'>
+        <Dialog.Content className='fixed top-1/2 w-[90%] max-w-[800px] rounded-lg shadow-md p-8 translate-x-[-50%] translate-y-[-50%] left-1/2 z-10 bg-gray-800'>
+          <Dialog.Title className='flex justify-between mb-4 font-bold text-gray-50'>
             <Dialog.Close className='flex justify-between w-full mb-4'>
               Adicionar indústria
               <MdClose className='text-xl' />
@@ -53,7 +53,7 @@ const AddIndustry = () => {
           <form className='flex flex-col w-full gap-4' onSubmit={handleIndustry}>
             <input
               type='text'
-              className='p-2 bg-gray-300 rounded-md shadow-sm'
+              className='w-full p-2 bg-gray-900 rounded-lg text-gray-50'
               placeholder='Nome Fantasia'
               value={fantasyName}
               onChange={(e) => {
@@ -62,7 +62,7 @@ const AddIndustry = () => {
             />
             <input
               type='text'
-              className='p-2 bg-gray-300 rounded-md shadow-sm'
+              className='w-full p-2 bg-gray-900 rounded-lg text-gray-50'
               placeholder='Razão Social'
               value={socialName}
               onChange={(e) => {
@@ -71,7 +71,7 @@ const AddIndustry = () => {
             />
             <input
               type='text'
-              className='p-2 bg-gray-300 rounded-md shadow-sm'
+              className='w-full p-2 bg-gray-900 rounded-lg text-gray-50'
               placeholder='CNPJ'
               value={cnpj}
               onChange={(e) => {
@@ -80,7 +80,7 @@ const AddIndustry = () => {
             />
             <input
               type='submit'
-              className='p-2 font-medium text-white rounded-md shadow-sm cursor-pointer bg-gradient-to-r from-blue-800 to-blue-600'
+              className='p-2 font-bold bg-blue-600 rounded-md shadow-sm cursor-pointer text-gray-50'
               value={'Adicionar'}
             />
           </form>
