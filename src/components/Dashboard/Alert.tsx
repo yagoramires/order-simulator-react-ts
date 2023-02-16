23d1866a47c148c15c463eac4bfdf30138a83cc8
yagox <yago.ramiresx@gmail.com>
2023-02-15 21:31:13 -0300

    styles: ajuste no component de alerta

--- src/components/Dashboard/Alert.tsx
+++ src/components/Dashboard/Alert.tsx
@@ -46,21 +46,21 @@ const Alert = ({ data }: AlertProps) => {
       </AlertDialog.Trigger>
       <AlertDialog.Portal>
         <AlertDialog.Overlay className='fixed dialogAnimationOverlay' />
-        <AlertDialog.Content className='fixed bg-white rounded-md shadow-md focus:outline-none top-1/2 left-1/2 transform translate-x-[-50%] translate-y-[-50%] w-[90%] max-w-[500px] max-h-[85vh] p-6 dialogAnimationContent'>
+        <AlertDialog.Content className='fixed  bg-gray-800 rounded-lg shadow-md focus:outline-none top-1/2 left-1/2 transform translate-x-[-50%] translate-y-[-50%] w-[90%] max-w-[500px] max-h-[85vh] p-6 dialogAnimationContent text-gray-50'>
           <AlertDialog.Title className='text-[17px] font-bold'>Você tem certeza?</AlertDialog.Title>
-          <AlertDialog.Description className='mb-5 text-[15px]'>
+          <AlertDialog.Description className='mb-5 text-[15px] text-gray-500'>
             Esta ação não poderá ser desfeita. Isso irá remover o item permanentemente dos nossos
             servidores.
           </AlertDialog.Description>
           <div style={{ display: 'flex', gap: 25, justifyContent: 'flex-end' }}>
             <AlertDialog.Cancel asChild>
-              <button className='flex items-center justify-center px-4 font-medium text-blue-600 transition-all duration-200 border-2 border-solid rounded cursor-pointer border-zinc-300 focus: hover:bg-zinc-200 h-9'>
+              <button className='flex items-center justify-center px-4 font-medium bg-blue-600 rounded cursor-pointer h-9'>
                 Cancelar
               </button>
             </AlertDialog.Cancel>
             <AlertDialog.Action asChild>
               <button
-                className='flex items-center justify-center px-4 font-medium text-white transition-all duration-200 bg-red-500 border-2 border-red-600 border-solid rounded shadow-md cursor-pointer focus: hover:bg-red-400 h-9'
+                className='flex items-center justify-center px-4 font-medium bg-red-500 rounded cursor-pointer h-9'
                 onClick={handleDelete}
               >
                 Sim, remover
