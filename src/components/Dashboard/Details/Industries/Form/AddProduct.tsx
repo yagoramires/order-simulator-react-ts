cfe4a7b4dcf5b6e016f35a0988f877ef70855011
yagox <yago.ramiresx@gmail.com>
2023-02-15 22:23:45 -0300

    style: ajuste no estilo do form de add produto

--- src/components/Dashboard/Details/Industries/Form/AddProduct.tsx
+++ src/components/Dashboard/Details/Industries/Form/AddProduct.tsx
@@ -71,8 +71,8 @@ const AddProduct = () => {
       </Dialog.Trigger>
       <Dialog.Portal>
         <Dialog.Overlay className='fixed inset-0 bg-black/80' />
-        <Dialog.Content className='fixed bg-white top-1/2 w-[90%] max-w-[800px] rounded-md shadow-lg p-8 translate-x-[-50%] translate-y-[-50%] left-1/2'>
-          <Dialog.Title className='flex justify-between mb-4 font-bold text-blue-600'>
+        <Dialog.Content className='fixed top-1/2 w-[90%] max-w-[800px] rounded-lg shadow-md p-8 translate-x-[-50%] translate-y-[-50%] left-1/2 z-10 bg-gray-800'>
+          <Dialog.Title className='flex justify-between mb-4 font-bold text-gray-50'>
             <Dialog.Close className='flex justify-between w-full mb-4'>
               Adicionar produto
               <MdClose className='text-xl' />
@@ -90,33 +90,33 @@ const AddProduct = () => {
           <form className='flex flex-col w-full gap-4' onSubmit={handleClient}>
             <input
               type='file'
-              className='w-full p-2 bg-gray-300 rounded-md shadow-sm'
+              className='w-full p-2 bg-gray-900 rounded-lg text-gray-50'
               onChange={handleSelectImage}
             />
             <input
               type='text'
-              className='p-2 bg-gray-300 rounded-md shadow-sm'
+              className='w-full p-2 bg-gray-900 rounded-lg text-gray-50'
               placeholder='Código'
               value={code}
               onChange={(e) => setCode(e.target.value)}
             />
             <input
               type='text'
-              className='p-2 bg-gray-300 rounded-md shadow-sm'
+              className='w-full p-2 bg-gray-900 rounded-lg text-gray-50'
               placeholder='Nome'
               value={name}
               onChange={(e) => setName(e.target.value)}
             />
             <input
               type='number'
-              className='p-2 bg-gray-300 rounded-md shadow-sm'
+              className='w-full p-2 bg-gray-900 rounded-lg text-gray-50'
               placeholder='Preço'
               value={price}
               onChange={(e) => setPrice(e.target.value)}
             />
             <input
               type='text'
-              className='p-2 bg-gray-300 rounded-md shadow-sm'
+              className='w-full p-2 bg-gray-900 rounded-lg text-gray-50'
               placeholder='Linha'
               value={family}
               onChange={(e) => setFamily(e.target.value)}
@@ -124,7 +124,7 @@ const AddProduct = () => {
 
             <input
               type='submit'
-              className='p-2 font-medium text-white rounded-md shadow-sm cursor-pointer bg-gradient-to-r from-blue-800 to-blue-600'
+              className='p-2 font-bold bg-blue-600 rounded-md shadow-sm cursor-pointer text-gray-50'
               value={'Adicionar'}
             />
           </form>
