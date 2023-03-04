ed32d4f3a7b55ca32e7597b87d9765c68495f34a
yagox <yago.ramiresx@gmail.com>
2023-03-03 21:55:26 -0300

    refact: remoção dos dados do cliente na pagina de pedido

--- src/components/Order/SelectedItems.tsx
+++ src/components/Order/SelectedItems.tsx
@@ -9,8 +9,9 @@ interface ClientProps {
 
 const SelectedItems = ({ client, industry }: ClientProps) => {
   return (
-    <div className='bg-gray-900 w-full max-w-[1400px] p-2  text-gray-50 rounded-lg'>
-      <div className='flex items-start justify-center w-full gap-2'>
+    <div className='absolute bottom-3 right-3'>
+      {/* <div className='bg-gray-900 w-full max-w-[1400px] p-2  text-gray-50 rounded-lg'> */}
+      {/* <div className='flex items-start justify-center w-full gap-2'>
         <div className='relative w-[50%]'>
           <span className='text-xs text-gray-500 lg:text-sm'>Cliente</span>
           <span className='flex items-center justify-between w-full text-xs uppercase break-words rounded-lg lg:text-sm'>
@@ -25,9 +26,9 @@ const SelectedItems = ({ client, industry }: ClientProps) => {
             {client.deadline}
           </span>
         </div>
-      </div>
+      </div> */}
 
-      <div className='flex items-center justify-end mt-2'>
+      <div className='flex items-center justify-end mt-2 text-gray-50'>
         {/* <div className='flex flex-col'>
           <span className='text-xs text-zinc-500'>Total</span>
           <span className='font-bold'>
